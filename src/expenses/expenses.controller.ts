cb509502a5712f98f8a0edf57ff89142ee6f62c1
Giorgi Cxovrebashvili <giorgicxovrebashvili@Giorgis-MacBook-Air.local>
2024-07-13 22:20:13 +0400

    final changes

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,37 +1,52 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
 import { ExpenseDto } from './dtos/expense.dto';
 import { ExpensesGuard } from './guards/expenses.guard';
+import { ApiKeyGuard } from './guards/expenseApiKey.guard';
 
 @Controller('expenses')
 export class ExpensesController {
-    constructor(private readonly expensesService: ExpensesService) { }
-    
-    @Get()
-    findAllExpenses() {
-        return this.expensesService.findAllExpenses();
-    }
+  constructor(private readonly expensesService: ExpensesService) {}
 
-    @Get(':id')
-    findOneExpense(@Param('id', ParseIntPipe) id: string) {
-        return this.expensesService.findOneExpense(+id);
-    }
+  @Get()
+  @UseGuards(ApiKeyGuard)
+  findAllExpenses() {
+    return this.expensesService.findAllExpenses();
+  }
 
-    @Post('/add')
-    @UseGuards(ExpensesGuard)
-    createExpense(@Body() body: ExpenseDto) { 
-        return this.expensesService.createExpense(body);
-    }
+  @Get(':id')
+  findOneExpense(@Param('id', ParseIntPipe) id: string) {
+    return this.expensesService.findOneExpense(+id);
+  }
 
-    @Put(':id')
-    @UseGuards(ExpensesGuard)
-    updateExpenses(@Param('id', ParseIntPipe) id: string, @Body() body: ExpenseDto) {
-        return this.expensesService.updateExpenses(id, body);
-    }
+  @Post('/add')
+  @UseGuards(ExpensesGuard)
+  createExpense(@Body() body: ExpenseDto) {
+    return this.expensesService.createExpense(body);
+  }
 
-    @Delete(':id')
-    @UseGuards(ExpensesGuard)
-    removeExpense(@Param('id', ParseIntPipe) id: string) { 
-        return this.expensesService.removeExpense(id);
-    }
+  @Put(':id')
+  @UseGuards(ExpensesGuard)
+  updateExpenses(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() body: ExpenseDto,
+  ) {
+    return this.expensesService.updateExpenses(id, body);
+  }
+
+  @Delete(':id')
+  @UseGuards(ExpensesGuard)
+  removeExpense(@Param('id', ParseIntPipe) id: string) {
+    return this.expensesService.removeExpense(id);
+  }
 }
---
+++ src/expenses/guards/expenseApiKey.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+export class ApiKeyGuard implements CanActivate {
+    private readonly apiKeyValue = '5511442233';
+    canActivate(
+      context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+      const request = context.switchToHttp().getRequest();
+      const apiKey = request.headers['api-key'];
+      if (this.apiKeyValue === apiKey) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
